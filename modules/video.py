ded868e2da09ac78d3ee7a57d370ad8bd3d88783
yiye <yiye.js@gmail.com>
2023-05-03 16:31:03 +0800

    feat: 完成基本的替换逻辑

--- modules/video.py
+++ modules/video.py
@@ -1,35 +1,44 @@
 import cv2
 import os
+import datetime
 import shutil
 
 class VideoProcessor:
-    def __init__(self,video_path, output_dir) -> None:
+    def __init__(self, output_dir) -> None:
 
-        self.video_path = video_path
+        self.video_path = None
         self.output_dir = output_dir
         self.temp_dir = os.path.join(output_dir, "temp")
         
-        if not os.path.exists(self.video_path):
-            raise Exception("Video path does not exist") 
-           
         self.clearOoutputDir()
         
-        self.cap = cv2.VideoCapture(self.video_path)
+        self.cap = None
+        self.blink_frames = None
     
-    def clearOoutputDir(self):   
-        shutil.rmtree(self.output_dir,ignore_errors=True)
-        os.makedirs(self.output_dir)
+    def clearOoutputDir(self):
+        if not os.path.exists(self.output_dir):
+            os.makedirs(self.output_dir)
+        if os.path.exists(self.temp_dir):
+            shutil.rmtree(self.temp_dir,ignore_errors=True)
         os.makedirs(self.temp_dir)  
 
     def release(self):
         self.cap.release()
+        self.video_path = None
+        self.cap = None
+        self.blink_frames = None
         cv2.destroyAllWindows()
+
+    def loadVideo(self, video_path):
+        if not os.path.exists(video_path):
+            raise Exception("Video path does not exist") 
+        self.video_path = video_path
+        self.cap = cv2.VideoCapture(self.video_path)
 
     def getFrames(self, interval=8, druation=0.3):
         extracted_frames = []
 
         fps = int(self.cap.get(cv2.CAP_PROP_FPS))
-        total_frames = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT)) 
 
         # calculate number of frames to extract every interval  
         num_frames = int(fps * druation)
@@ -65,7 +74,51 @@ class VideoProcessor:
                     "image": image,
                     "frame_path": frame_path
                 })
-    
+
+        self.blink_frames = extracted_frames
         return extracted_frames
 
+    def replaceFrames(self, images):
+        width = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        height = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))   
+        fps = int(self.cap.get(cv2.CAP_PROP_FPS))
+
+        # set video encoder
+        fourcc_code = int(self.cap.get(cv2.CAP_PROP_FOURCC))
+        # fourcc_str = "".join([chr((fourcc_code >> 8 * i) & 0xFF) for i in range(4)])
+
+        now = datetime.datetime.now()   
+        format_now = now.strftime("%Y-%m-%d_%H-%M-%S")
+        output_video_name = f"video_{format_now}.mp4"
+        output_video_path = os.path.join(self.output_dir, output_video_name)
+
+        output_video = cv2.VideoWriter(output_video_path, fourcc_code, fps, (width, height))
+
+        # cosntruct a dict of frame index and image
+        replacement_frames = {}
+        for index, image in enumerate(images):
+            if image is None:
+                break
+
+            frame = self.blink_frames[index]
+            if frame is None:
+                break
+            resized_image = cv2.resize(image, (width, height), interpolation=cv2.INTER_AREA)   
+            replacement_frames[frame["index"]] = resized_image  
+            
+        # write self.cap frames to output video, if frame index is in images, replace it with image
+        self.cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+        while True:
+            success, frame = self.cap.read()
+            if not success:
+                break
+            frame_index = int(self.cap.get(cv2.CAP_PROP_POS_FRAMES))
+            if frame_index in replacement_frames:
+                frame = replacement_frames[frame_index]   
+                
+            output_video.write(frame)   
+        
+        output_video.release()
+        
+        return output_video_path   
 
--- splite-video.py
+++ splite-video.py
@@ -2,26 +2,40 @@ from modules.video import VideoProcessor
 import os
 import gradio as gr
 
-output_dir = os.path.join(os.getcwd(), "output")
 max_images = 12
-
+output_dir = os.path.join(os.getcwd(), "output")
+video_processor = VideoProcessor(output_dir=output_dir)
 
 def splite_video(video, interval, druation):
     print(f"video path: {video}")
     print(f"blink: interval {interval}， druation {druation}")
-    video_processor = VideoProcessor(video_path=video, output_dir=output_dir)
-    frames = video_processor.getFrames(interval=interval, druation=druation)
-    video_processor.release()
-    images = [frame["image"] for frame in frames]
+    
+    global video_processor
+    video_processor.loadVideo(video)
+    video_processor.getFrames(interval=interval, druation=druation)
+
+    images = [frame["image"] for frame in video_processor.blink_frames]
 
     return images
 
+def replace_video(*image_inputs):
+    # if video_processor or blink_frames is None, print error
+    if video_processor.blink_frames is None:
+        print("please splite video first")
+        return  
+    
+    # replace blink frame with selected images  
+    new_video_path = video_processor.replaceFrames(image_inputs)
+    # video_processor.release()
+
+    return new_video_path
 
 with gr.Blocks() as app:
     image_inputs = []
+    state = gr.State({"video_processor": None, "blink_frames": None})    
     with gr.Row():
         with gr.Column():
-            with gr.Row().style(height=400):
+            with gr.Box():
                 video_input = gr.Video(label="Select a Video")
             with gr.Row():
                 interval_input = gr.Slider(value=5, minimum=1, maximum=10, step=1, label="Blink Interval(s)")
@@ -29,9 +43,11 @@ with gr.Blocks() as app:
             with gr.Row():
                 splite_button = gr.Button(value="Splite Video")
                 replace_button = gr.Button(value="Replace Frames")
-        
+            with gr.Box():  
+                relaced_video_output = gr.Video(label="Replaced Video")
+
         with gr.Column():
-            with gr.Row():
+            with gr.Box():
                 caped_images_output = gr.Gallery(label="caped_images").style(columns=4, height="auto", object_fit="contain")
             with gr.Box():
                 with gr.Row():
@@ -40,10 +56,15 @@ with gr.Blocks() as app:
                     for i in range(max_images):
                         image_input = gr.Image(label=f"{i}")
                         image_inputs.append(image_input)
+            
 
-
+    # extract blink frame from video
     ips = [video_input, interval_input, duration_input]
     ops = caped_images_output 
     splite_button.click(splite_video,ips, ops)
+    # replace blink frame with selected images
+    ips = image_inputs
+    ops = relaced_video_output
+    replace_button.click(replace_video, ips, ops)
 
 app.launch(inline=False, debug=True)
